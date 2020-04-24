6d4e31aee9f07e902edcb2e14aa6fb53d86f5169
Simon <simonmueller.ch@gmail.com>
2020-04-24 15:16:45 +0200

    add sliders

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import numpy as np
 from rona import Covid
 
@@ -30,7 +31,7 @@ def generate_table(dataframe, max_rows=10):
                 ]
             ),
         ],
-        style={"color": colors["text"]}
+        style={"color": colors["text"]},
     )
 
 
@@ -53,11 +54,67 @@ app.layout = html.Div(
             children="built with Dash: A web application framework  for Python.",
             style={"textAlign": "center", "color": colors["text"]},
         ),
-        dcc.Graph(id="Enhanced SEIR model for the spread of corona", figure=fig,),
+        dcc.Graph(id="rona", figure=fig,),
+        html.Div(
+            [
+                dcc.Slider(
+                    id="R0-slider",
+                    min=1,
+                    max=9,
+                    value=2,
+                    marks={str(i): str(i) for i in range(1, 10, 2)},
+                    step=0.1,
+                ),
+                html.Div(
+                    id="slider-output-container",
+                    style={"textAlign": "center", "color": colors["text"]},
+                ),
+                dcc.Slider(
+                    id="intervention-slider",
+                    min=0,
+                    max=1,
+                    value=0.66,
+                    marks={str(i): str(i) for i in [0, 1]},
+                    step=0.1,
+                ),
+                html.Div(
+                    id="slider2-output-container",
+                    style={"textAlign": "center", "color": colors["text"]},
+                ),
+            ],
+            style={"width": "20%", "display": "inline-block", "padding": "0 20"},
+        ),
         html.H4(children="Summary", style={"color": colors["text"]}),
         generate_table(df_sum),
     ],
 )
+
+
+@app.callback(
+    [
+        Output("rona", "figure"),
+        Output("slider-output-container", "children"),
+        Output("slider2-output-container", "children"),
+    ],
+    [Input("R0-slider", "value"), Input("intervention-slider", "value")],
+)
+def update_figure(R0, OMInterventionAmt):
+    y0 = np.array([N0, I0])
+    params = np.array([R0, OMInterventionAmt, 5.2, 2.9, 60])
+    cv = Covid(y0, params)
+    cv.solve(tmin, tmax)
+    df, df_sum, fig = cv.plot_plotly()
+
+    fig.update_layout(
+        plot_bgcolor=colors["background"],
+        paper_bgcolor=colors["background"],
+        font={"color": colors["text"]},
+    )
+
+    sliderText = "R0 = {:.1f}".format(R0)
+    slider2Text = "Intervention: decrease transmission by {:.0f}%".format(100 * OMInterventionAmt)
+    return (fig, sliderText, slider2Text)
+
 
 if __name__ == "__main__":
     app.run_server(debug=True, dev_tools_hot_reload=True)
